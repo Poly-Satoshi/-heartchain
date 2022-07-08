68cf3b9c17f1e88738989c21aaa6d1cca734afd4
Mylo Cyrus <irahiggins740@gmail.com>
2022-07-08 08:35:16 -0700

    Update Production_active.tf

--- Production_active.tf
+++ Production_active.tf
@@ -1,17 +1,29 @@
-"
+/n
 
-  name = "my-vpc"
+ /n
+/n
+/n
+/n
+/n
+/n name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+ /n azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+/n  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+ /n public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+/n  enable_nat_gateway = true
+/n  enable_vpn_gateway = true
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
+ /n
+
+ /n
+/n 
+/nTerraform = "true"
+  /n  Environment = "dev"
+/n
+/n
+/n tags = {
+    
+  
+
